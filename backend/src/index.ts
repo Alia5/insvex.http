bd17d58d3a90b59a74e74f65508afe47b5dc3cb5
Peter Repukat <orly.alia5@gmail.com>
2023-08-11 00:03:51 +0200

    Prep serviceInit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import { Logger, setJwtOptions, setJwtSecrets } from "fliessheck";
+import { Logger, initServices, setJwtOptions, setJwtSecrets } from "fliessheck";
 import * as http from 'http';
-import { handler } from "insvex.http-frontend/handler";
+import { handler as frontendHandler } from "insvex.http-frontend/handler";
 
 const main = async () => {
     const port = 3000 as const;
@@ -16,7 +16,9 @@ const main = async () => {
     setJwtSecrets('ChangeMySuperDuperSecret');
     setJwtOptions({  expiresIn: '1d' });
 
-    expressApp.use(handler);
+    expressApp.use(frontendHandler);
+
+    initServices([], expressApp, undefined)
 
     httpServer.listen(port, () => {
         Logger.Info('HTTPserver', `Server listening on port ${port}`);
