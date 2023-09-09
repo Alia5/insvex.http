e0967e98570eaed2ee9f7cfe1d603aea4038b0ac
Peter Repukat <orly.alia5@gmail.com>
2023-09-09 04:09:21 +0200

    Soother linter

--- backend/src/svelteKit.env.d.ts
+++ backend/src/svelteKit.env.d.ts
@@ -1,3 +1,4 @@
 declare module 'insvex.http-frontend/handler' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     export const handler: (req: any, res: any, next: any) => any;
 }
